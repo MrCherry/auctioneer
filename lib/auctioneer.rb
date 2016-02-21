b8091d99939f96fe6553880b5754804186f7a28b
Фёдор Черашев <fc@voltmobi.com>
2016-02-21 16:57:54 +0300

    First class and test

---
+++ lib/auctioneer.rb
@@ -0,0 +1,9 @@
+class Auctioneer
+  def initialize
+
+  end
+
+  protected
+
+
+end
---
+++ test/auctioneer_test.rb
@@ -0,0 +1,7 @@
+require_relative '../lib/auctioneer'
+
+class AuctioneerTest < Minitest::Test
+  def test_run
+    assert_equal '1', '1'
+  end
+end
