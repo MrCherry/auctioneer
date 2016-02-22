e03c80eb6e65d55c919340df9823f5bde63eaaa0
Фёдор Черашев <fc@voltmobi.com>
2016-02-23 01:31:17 +0300

    auction function implementation + test

--- lib/auctioneer.rb
+++ lib/auctioneer.rb
@@ -1,9 +1,38 @@
-class Auctioneer
-  def initialize
+module Auctioneer
+  # @param [Array] creatives
+  # @param [Integer] number_of_winners
+  # @param [String] country_name
+  # @return [Array] array of winners of the auction
+  def self.auction(creatives:, number_of_winners:, country_name: nil)
+    # Check that passed parameters are valid
+    raise Exception.new('creatives parameter must be specified') unless creatives.is_a?(Array)
+    raise Exception.new('number_of_winners parameter must be specified') unless number_of_winners.is_a?(Integer)
 
+    # Init contenders and winners arrays
+    contenders = creatives.dup
+    winners = []
+
+    # Select only specified country's contenders
+    contenders.select!{ |c| c['country_name'] == country_name || c['country_name'].nil? } if country_name
+
+    # Process contenders array
+    number_of_winners.times do
+      break if contenders.empty?
+
+      # Generate a random contender's index based on contenders array size
+      index = rand(contenders.count)
+
+      # Select a winner
+      winner = contenders.delete_at(index)
+
+      # Reject contenders with the same advertiser_id
+      contenders.reject!{ |c| c['advertiser_id'] == winner['advertiser_id'] }
+
+      # Add the winner to the winners array
+      winners << winner
+    end
+
+    # Return the winners array
+    winners
   end
-
-  protected
-
-
 end
--- test/auctioneer_test.rb
+++ test/auctioneer_test.rb
@@ -2,7 +2,59 @@ require './test/test_helper'
 require './lib/auctioneer'
 
 class AuctioneerTest < Minitest::Test
-  def test_run
-    assert_equal '1', '1'
+  def setup
+    fixture = File.read(File.expand_path('creatives.json', 'test/fixtures'))
+    @creatives = JSON.load(fixture)
+    @unique_advertises_count = @creatives.uniq{ |c| c['advertiser_id'] }.count
+  end
+
+  # Auctioneer.auction should consider number_of_winners parameter
+  def test_number_of_winners
+    Range.new(1, @unique_advertises_count).each do |i|
+      winners = Auctioneer.auction(
+          creatives: @creatives,
+          number_of_winners: i
+      )
+
+      assert { winners.count == i }
+    end
+  end
+
+  # Auctioneer.auction should consider country_name parameter if it is provided
+  def test_country_name_passed
+    unique_countries = @creatives.map{ |c| c['country_name'] }.compact.uniq
+
+    unique_countries.each do |country_name|
+      winners = Auctioneer.auction(
+          creatives: @creatives,
+          number_of_winners: @unique_advertises_count,
+          country_name: country_name
+      )
+
+      winners.each do |winner|
+        assert { winner['country_name'] == country_name || winner['country_name'].nil? }
+      end
+    end
+  end
+
+  # Auctioneer.auction should consider country_name parameter if it is nil
+  def test_country_name_nil
+    winners = Auctioneer.auction(
+        creatives: @creatives,
+        number_of_winners: @unique_advertises_count
+    )
+
+    assert { winners.select{ |c| c['country_name'].nil? } }
+  end
+
+  # All winners of Auctioneer.auction have unique advertiser_id
+  def test_unique_advertiser_id
+    winners = Auctioneer.auction(
+        creatives: @creatives,
+        number_of_winners: @unique_advertises_count
+    )
+
+    advertisers = winners.map{ |c| c['advertiser_id'] }
+    assert { advertisers.count == advertisers.uniq.count }
   end
 end
